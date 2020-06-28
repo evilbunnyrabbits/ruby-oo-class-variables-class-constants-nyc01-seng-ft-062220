6ebe86c4f8abd35589361bd20b7f8bd66cdb9ca8
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-06-28 13:34:12 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,14 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = [].to_set
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -11,4 +16,10 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brands=(brand)
+    BRANDS << brand
+
+  end
 end
+
+
